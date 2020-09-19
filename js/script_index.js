72f8831d6474d5fe4ab417333ede6c34055087e2
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2020-09-19 16:23:09 +0500

    Add working news description.

--- js/script_index.js
+++ js/script_index.js
@@ -1,11 +1,40 @@
 let position = 0;
 
+let text = document.getElementsByClassName('news_description');
 let news = document.getElementsByClassName('news');
 let circles = document.getElementsByClassName('circle');
-switchingNews(position);
+
+for (let i=0;i<news.length;i++){
+		news[i].onmouseenter = showText;
+		news[i].onmouseleave = hideText;
+}
 
 document.getElementsByClassName('left')[0].onclick = switchingLeft;
 document.getElementsByClassName('right')[0].onclick = switchingRight;
+
+switchingNews(position);
+
+function showText(){
+	console.log("show");
+	text[position].style.display = "block";
+}
+
+function hideText(){
+	console.log("hide");
+	text[position].style.display = "none";
+}
+
+function viewNews(n){
+	news[n].style.display = "block";
+	circles[n].style.background = '#53110c';
+};
+
+function hideNews(){
+	for (let i=0;i<news.length;i++){
+		news[i].style.display = "none";
+		circles[i].style.background = '#facdce';
+	}
+};
 
 function switchingNews(n){
 	hideNews();
